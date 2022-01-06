cd76f3608cd579706adf14661994cc9319f88075
0dataloss <github@t-hoster.com>
2022-01-06 16:47:04 +0000

    First working version

--- CountS3.py
+++ CountS3.py
@@ -21,6 +21,9 @@ cSvPrintadd = ""
 # Read Env Variables
 if os.getenv('IONOS_APIKEY'):
   if os.getenv('IONOS_APIKEYSECRET'):
+    usernameio = os.getenv('IONOS_USERNAME')
+    passwordio = os.getenv('IONOS_PASSWORD')
+    contractio = os.getenv('IONOS_CONTRACT')
     apiKeyi = os.getenv('IONOS_APIKEY')
     apiSecretKeyi = os.getenv('IONOS_APIKEYSECRET')
 else:
@@ -28,6 +31,9 @@ else:
     # File Exist so  Import .ionos.cfg
     sys.path.append("ionos")
     import ionos as i
+    usernameio=i.username
+    contractio=i.contract
+    passwordio=i.password
     apiKeyi=i.apikey
     apiSecretKeyi=i.apisecretkey
   else:
@@ -37,15 +43,54 @@ else:
 
 # check if Key and secret Key have values
 try:
-    apiKeyi
+  usernameio
+  user_input_username = usernameio
 except:
-    print("API Key missing, please export it in your env with 'export IONOS_APIKEY=<apikey>'"\n
-    "Or set up the file ionos.py")
+  user_input_username = input("Please insert username\n")
 try:
-    apiSecretKeyi
+  isinstance
+  contract = contractio
 except:
-    print("API Key missing, please export it in your env with 'export IONOS_APIKEYSECRET=<apiSecretKeyi>'"\n
-    "Or set up the file ionos.py")
+  user_input_contract = input("Please insert contract number\n")
+  contract = user_input_contract
+try:
+  isinstance
+  user_input_password=passwordio
+except:
+  user_input_password=getpass()
+try:
+  apiKeyi
+except:
+  print(f"API Key missing, please export it in your env with 'export IONOS_APIKEY=<apikey>'\nOr set up the file ionos.py")
+try:
+  apiSecretKeyi
+except:
+  print(f"API Key missing, please export it in your env with 'export IONOS_APIKEYSECRET=<apiSecretKeyi>'\nOr set up the file ionos.py")
+
+# Prepare base46 username:password Headers
+user_input_usernameandpassword=str(user_input_username+":"+user_input_password)
+message_bytes = user_input_usernameandpassword.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+token = base64_bytes.decode('ascii')
+contract=contract
+authAcc={"Authorization": "Basic "+token+""}
+
+# Buld a catalog for all the products with ID, the price per unit, and the unit
+def catalog(authHead,contract):
+  url = "https://api.ionos.com/billing/"+ contract +"/products"
+  response = requests.get(url, headers=authHead)
+  catalogRp = (response.json())
+  # only return the products with deprected = False
+  catalogRpOnlyFalse = []
+  catalogProducts = catalogRp['products']
+  # Build a new list of Dict from previous Dict for only services Not Deprecated
+  for item in catalogProducts:
+    if item['deprecated'] is False:
+      catalogRpOnlyFalse.append(item)
+  return catalogRpOnlyFalse
+
+# Start Building the Catalog
+catalogNotDeprecated=catalog(authAcc,contract)
 
 ##
 # Create S3 Objects
@@ -62,6 +107,7 @@ s3resource = boto3.resource('s3',
 )
 totalSizeByte = 0
 buckets = s3client.list_buckets()
+print(f"{buckets}")
 for bucket in buckets['Buckets']:
   s3sizeByte=0
   bucket42 = s3resource.Bucket(bucket['Name'])
@@ -88,6 +134,8 @@ for bucket in buckets['Buckets']:
     bucket42 = s3resource.Bucket(bucket['Name'])
   for s3_file in bucket42.objects.all():
     s3sizeByte = s3sizeByte + s3_file.size
+  bucketSize = s3sizeByte / 1024 / 1024 / 1024
+  print(f"{bucket42.name}:{bucketSize}")
   totalSizeByte = (totalSizeByte + s3sizeByte)
   s3sizeByte = s3sizeByte / 1024 / 1024 / 1024
 totalSizeByte=totalSizeByte/1024/1024/1024
@@ -98,8 +146,8 @@ for price in catalogNotDeprecated:
       consumedUnit=float(totalSizeByte)
       spending=(totalSizeByte*unitCost)
 
-      cSvPrint="S3",s3DcUuid,price['meterDesc'],price['meterId'],price['unitCost']['quantity'],price['unitCost']['unit'],totalSizeByte,spending,price['unitCost']['unit']
+#      cSvPrint="S3",s3DcUuid,price['meterDesc'],price['meterId'],price['unitCost']['quantity'],price['unitCost']['unit'],totalSizeByte,spending,price['unitCost']['unit']
+      cSvPrint="S3",price['meterDesc'],price['meterId'],price['unitCost']['quantity'],price['unitCost']['unit'],totalSizeByte,spending,price['unitCost']['unit']
       cSvPrint = str(cSvPrint)
       cSvPrintadd = cSvPrint.strip("[()]")+cSvPrintadd
 print(cSvPrintadd)
-print(f"\n\n--------\nGrand Total Compute & Network --> {grandTotal}\n--------")
